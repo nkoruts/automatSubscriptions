b72f066d2f3be27866b77230184d600cc23b7903
Nikita Koruts <nikitakoruts@gmail.com>
2026-02-05 11:02:04 +0200

    Start server logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/nkoruts/automatSubscriptions/internal/server"
+	"github.com/nkoruts/automatSubscriptions/internal/subscription"
+)
+
+func main() {
+	subsList := subscription.NewList()
+	httpHandlers := server.NewHTTPHandlers(subsList)
+	httpServer := server.NewHTTPServer(httpHandlers)
+
+	if err := httpServer.StartServer(); err != nil {
+		fmt.Println("HTTP server error:", err)
+	}
 }
